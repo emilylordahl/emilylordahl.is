8de941f9a7a452c045c7d6b673a2aa57d8a113a0
Emily Lordahl <emilylordahl@Emilys-MBP.home>
2017-03-14 23:26:02 -0400

    messing around. adding typing indicator for different subdomains and corresponding css for icons above the fold.

--- js/init.js
+++ js/init.js
@@ -4,15 +4,15 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
-    function switchColor () {
-      var colors = ['white', 'lightgray', 'dimgray', 'salmon', 'salmon', 'white', 'white', 'blue', 'blue'];
-
-      if (length >= colors.length) length = 0;
-        $('h1.header').css('color', colors[length++]);
-        setTimeout(switchColor, 2500);
-    }
-
-  switchColor();
+  //   function switchColor () {
+  //     var colors = ['white', 'lightgray', 'dimgray', 'salmon', 'salmon', 'white', 'white', 'blue', 'blue'];
+  //
+  //     if (length >= colors.length) length = 0;
+  //       $('h1.header').css('color', colors[length++]);
+  //       setTimeout(switchColor, 2500);
+  //   }
+  //
+  // switchColor();
 
   // Smooth Scroll to Anchor Tags
   $('a[href*=#]:not([href=#])').click(function() {
@@ -33,12 +33,19 @@
   $(window).scroll(function(){
     removeActionClass();
     var windowTop = $(window).scrollTop() + $(".nav-wrapper").height();
+    var $logotxt = $("#logo-container");
+    var str = "emilylordahl.is";
     if (($("#contact").offset().top) < windowTop ) {
       $("nav ul a.nav-contact").addClass("active");
+      $logotxt.text(str + "/sharing");
     } else if (($("#about").offset().top) < windowTop ) {
      $("nav ul a.nav-about").addClass("active");
+     $logotxt.text(str + "/developing");
     } else if (($("#work").offset().top) < windowTop ) {
       $("nav ul a.nav-work").addClass("active");
+      $logotxt.text(str + "/designing");
+    } else {
+      $logotxt.text(str);
     }
   });
 
@@ -62,5 +69,60 @@
     $(this).find(".fa").css("margin-left", ".5rem");
   });
 
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
